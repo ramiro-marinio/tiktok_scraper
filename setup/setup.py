a5b4a64a9f9a408102d2ce774ea2b3c993336187
Ramiro <ramiro@viatu.com>
2025-02-03 19:04:30 -0300

    Funny change

--- setup/setup.py
+++ setup/setup.py
@@ -1,11 +1,14 @@
 """Includes function to set up the app."""
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+import presentation
 import constants
 import environment
 
 def setup():
     """Starts up the browser and returns its object."""
+    print(presentation.presentation()) #Cool presentation
+
     options = webdriver.ChromeOptions()
     options.add_argument(f"user-data-dir={environment.CHROME_USER_DATA_DIR}")
     options.add_argument(f'--profile-directory={environment.CHROME_PROFILE}')
